96ebb30e92cb3a3784a6f15890daf514d88b2bc2
boilerPlate <dltjdals100@gmail.com>
2022-08-11 17:32:55 +0900

    업데이트

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const app = express();
 const port = 5000;
 const { User } = require("./models/User");
 const bodyParser = require("body-parser");
-const config = require('./config/key');
+const config = require("./config/key");
 
 // application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -26,6 +26,7 @@ mongoose
   .catch((err) => console.log(err));
 
 // mongodb + srv://admin:<password>@boilerplate.mmoar.mongodb.net/?retryWrites=true&w=majority
+console.log(process.env.MONGO_URI);
 
 app.get("/", (req, res) => res.send("hello world! 안녕하세요 ~ ")); // req : request, res : response
 
@@ -40,7 +41,88 @@ app.post("/register", (req, res) => {
   });
 });
 
+app.post("/login", (req, res) => {
+  // 1. 요청된 이메일을 데이터베이스에 있는지 찾음
+  User.findOne({ email: req.body.email }, (err, user) => {
+    if (!user) {
+      return res.json({
+        loginSucess: false,
+        message: " 제공된 이메일에 해당하는 유저가 없습니다.",
+      });
+    }
+    // 2. 요청된 이메일이 데이터베이스에 있다면 비밀번호 체크
+    user.comparePassword(req.body.password, (err, isMatch) => {
+      if (!isMatch)
+        return res.json({
+          loginSucess: false,
+          message: "비밀번호가 틀렸습니다.",
+        });
+    });
+    // 3. 비밀번호가 맞다면 토큰을 생성하기
+  });
+});
+
+app.post("/login", (req, res) => {
+  User.findOne({ email: req.body.email }, (err, user) => {
+    if (!user) {
+      return res.json({
+        loginSucess: false,
+        message:"X",
+      })
+    }
+    user.comparePassword(req.body.password, (err, isMatch) => {
+      if (!is)
+    })
+  })
+})
+
+
 app.listen(port, () => console.log(`example ${port}`));
 
 // body-parser - 서버로 request하는 통신에 사용됨 (Client에서 보내는 정보를 서버에서 가져올 수 있도록 만들어줌)
 // postman - Client 역할을 해줌
+
+
+
+
+
+
+
+
+
+
+
+// call back 함수 이해하기(feat.동기비동기) ********************************
+
+// const callback = (number) => {
+//   console.log(number);
+// };
+
+// const add = (a, b, callback) => {
+//   // 동기 / 비동기
+//   // 1
+//   // 동기 -> 바로실행
+//   const result = a / b;
+
+//   // 비동기 -> 언제 결과가 나올지 모르는거
+//   const user = db.read(); // 비동기
+//   callback(err, user);
+
+//   return result;
+// };
+
+// const result = add(1, 2);
+
+// const asyncAdd = () =>
+//   new Promise((resolve, reject) => {
+//     setTimeout(() => {
+//       resolve(true);
+//     }, 1000);
+//   });
+
+// const asyncResult = async () => {
+//   const result = await asyncAdd() // 기다림
+//   console.log(result) // -> true
+
+//   return result;
+// }
--- models/User.js
+++ models/User.js
@@ -1,4 +1,6 @@
 const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
+const saltRounds = 10; // salt를 이용하여 비밀번호를 암호화 함
 
 const userSchema = mongoose.Schema({
   name: {
@@ -17,7 +19,7 @@ const userSchema = mongoose.Schema({
   lastname: {
     type: String,
     maxlength: 50,
-  }, 
+  },
   role: {
     type: Number,
     default: 0,
@@ -31,6 +33,34 @@ const userSchema = mongoose.Schema({
     type: Number,
   },
 });
+
+// 유저 정보를 저장하기 전에 특정 함수를 처리할 수 있음
+// next 함수를 통해 save로 넘길 수 있음
+userSchema.pre("save", function (next) {
+  var user = this;
+  // 비밀번호 암호화
+  if (user.isModified("password")) {
+    bcrypt.genSalt(saltRounds, function (err, salt) {
+      if (err) return next(err);
+
+      bcrypt.hash(user.password, salt, function (err, hash) {
+        if (err) return next(err);
+        user.password = hash;
+        next();
+      });
+    });
+  } else {
+    next();
+  }
+});
+
+// cb는 callback
+userSchema.methods.comparePassword = (plainPassword, cb) => {
+  bcrypt.compare(plainPassword, this.password, (err, isMatch) => {
+    if (err) return cb(err);
+    cb(null, isMatch);
+  });
+};
 
 const User = mongoose.model("User", userSchema);
 
